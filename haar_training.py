8ac267495db4d1b1e32fa75011cb61dfd238a05f
Jet Chung <jetchung1@gmail.com>
2019-10-30 12:30:01 -0400

    testing

--- haar_training.py
+++ haar_training.py
@@ -6,30 +6,24 @@ import cv2
 def show_webcam():
     fist = cv2.CascadeClassifier('aGest.xml')
     palm = cv2.CascadeClassifier('palm.xml')
-    aGest = cv2.CascadeClassifier('aGest.xml')
-    closed_frontal_palm = cv2.CascadeClassifier('closed_frontal_palm.xml')
 
     cam = cv2.VideoCapture(0)
     while True:
         ret_val, img = cam.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        detect = fist.detectMultiScale(gray, 1.3, 5)
-        for (x, y, w, h) in detect:
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        fist_detect = fist.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in fist_detect:
+            img = cv2.rectangle(img, (x-50, y-50), (x + w+50, y + h+50), (255, 0, 0), 2)
+            img = cv2.putText(img, 'fist', (x,y+h), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0),2)
 
+            roi = img[y:y + h, x:x + w]
+        palm_detect = palm.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in palm_detect:
+            img = cv2.rectangle(img, (x-50, y-50), (x + w+50, y + h+50), (255, 0, 0), 2)
+            img = cv2.putText(img, 'palm', (x,y+h), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0),2)
 
-
-            img = cv2.putText(img, 'palm',
-                              (x,y+h),
-                              cv2.FONT_HERSHEY_SIMPLEX,
-                        1,
-                              (255,0,0),
-                        2)
-
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_color = img[y:y + h, x:x + w]
-
+            roi = img[y:y + h, x:x + w]
 
         cv2.imshow("test", img)
 
