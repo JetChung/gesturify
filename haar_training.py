06e8ac7efb33083a4dfcba19120749e02596e9b4
Jet Chung <jetchung1@gmail.com>
2019-10-30 12:23:47 -0400

    testing

--- haar_training.py
+++ haar_training.py
@@ -4,30 +4,29 @@ import cv2
 
 
 def show_webcam():
-    fist = cv2.CascadeClassifier('fist.xml')
-    left = cv2.CascadeClassifier('left.xml')
-    right = cv2.CascadeClassifier('right.xml')
-    lpalm = cv2.CascadeClassifier('lpalm.xml')
-    rpalm = cv2.CascadeClassifier('rpalm.xml')
+    fist = cv2.CascadeClassifier('aGest.xml')
+    palm = cv2.CascadeClassifier('palm.xml')
+    aGest = cv2.CascadeClassifier('aGest.xml')
+    closed_frontal_palm = cv2.CascadeClassifier('closed_frontal_palm.xml')
 
     cam = cv2.VideoCapture(0)
     while True:
         ret_val, img = cam.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        fist = fist.detectMultiScale(gray, 1.3, 5)
-        for (x, y, w, h) in fist:
+        detect = fist.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in detect:
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_color = img[y:y + h, x:x + w]
-        left = left.detectMultiScale(gray, 1.3, 5)
-        for (x, y, w, h) in left:
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            roi_color = img[y:y + h, x:x + w]
-        right = right.detectMultiScale(gray, 1.3, 5)
-        for (x, y, w, h) in right:
-            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+
+
+            img = cv2.putText(img, 'palm',
+                              (x,y+h),
+                              cv2.FONT_HERSHEY_SIMPLEX,
+                        1,
+                              (255,0,0),
+                        2)
+
             roi_gray = gray[y:y + h, x:x + w]
             roi_color = img[y:y + h, x:x + w]
 
